df8601897413a19ae92bfd4a7e2f49406b0ff410
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2018-07-30 09:42:31 -0400

    closes #7

--- _framework/together.js
+++ _framework/together.js
@@ -1,4 +1,3 @@
-'use strict';
 
 var fs = require('fs-extra');
 
@@ -29,15 +28,26 @@ fs.ensureFileSync(envPath);
 //loading after sub apps to override any potential env vars
 require('dotenv').config({ path: envPath });
 
+//if none is provided, defaulted to 443
+process.env.EXPRESS_PORT = process.env.EXPRESS_PORT || 443;
+
 
 /* MOUNTING & STARTING */
 
 try {
 	app.use(subApi.path, subApi.app);
 	app.use(subApp.path, subApp.app);
-	app.listen(process.env.EXPRESS_PORT || 443);
 }
 catch(err) {
 	console.error(err);
 	console.error(`Whoops! Suspecting either EXPRESS_API_PATH or EXPRESS_APP_PATH weren't set in the api and app sub applications. Easy fix, go add these paths into your .env files. Also if you want the app to run on your root path, simply use / and prefix the api to use /api.`);
+}
+
+if (process.env.EXPRESS_PORT === 443) {
+	var key = fs.readFileSync(path.resolve(__dirname, 'key.pem')) || null;
+	var cert = fs.readFileSync(path.resolve(__dirname, 'cert.pem')) || null;
+	require('https').createServer({ key, cert }, app).listen(443);
+}
+else {
+	app.listen(process.env.EXPRESS_PORT);
 }
